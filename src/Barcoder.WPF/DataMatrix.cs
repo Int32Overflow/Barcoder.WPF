ed70d32e16ab0ee235e42451015191129a2c4f93
Frank Schmid <schmid@euroident.de>
2022-12-06 10:48:18 +0100

    Add Property "FixedColumnCount" in DataMatrix

--- src/Barcoder.WPF/DataMatrix.cs
+++ src/Barcoder.WPF/DataMatrix.cs
@@ -1,18 +1,25 @@
-using System.Windows;
-using System.Windows.Controls;
 using Barcoder.DataMatrix;
 using Barcoder.WPF.Base;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Barcoder.WPF
 {
     [TemplatePart(Name = CanvasElementName, Type = typeof(Canvas))]
     public class DataMatrix : Base2DCodeControl
     {
+        public static readonly DependencyProperty FixedColumnCountProperty = DependencyProperty.Register(nameof(FixedColumnCount), typeof(byte), typeof(DataMatrix), new FrameworkPropertyMetadata((byte)0, FrameworkPropertyMetadataOptions.None));
         public static readonly DependencyProperty FixedRowCountProperty = DependencyProperty.Register(nameof(FixedRowCount), typeof(byte), typeof(DataMatrix), new FrameworkPropertyMetadata((byte)0, FrameworkPropertyMetadataOptions.None));
 
         static DataMatrix()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(DataMatrix), new FrameworkPropertyMetadata(typeof(DataMatrix)));
+        }
+
+        public byte FixedColumnCount
+        {
+            get => GetValue<byte>(FixedColumnCountProperty);
+            set => SetValue(FixedColumnCountProperty, value);
         }
 
         public byte FixedRowCount
@@ -23,8 +30,11 @@ namespace Barcoder.WPF
 
         protected override IBarcode GetBarcode()
         {
-            int? fixedRowCount = FixedRowCount == 0 ? (int?)null : FixedRowCount;
-            return DataMatrixEncoder.Encode(Value ?? "", fixedRowCount);
+            int? fixedRowCount = FixedRowCount == 0 ? null : FixedRowCount;
+            int? fixedColumnCount = FixedRowCount == 0 ? null : FixedRowCount;
+            if (fixedColumnCount == null || fixedRowCount == null)
+                fixedRowCount = fixedColumnCount = null;
+            return DataMatrixEncoder.Encode(Value ?? "", fixedRowCount, fixedColumnCount);
         }
 
         protected override IBarcode GetErrorBarcode()
